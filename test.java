fe9bc90b56deec30c2d3a73e4b60a048bb0b7697
manoj0250 <47047961+manoj0250@users.noreply.github.com>
2019-02-15 14:23:55 +0530

    Update test.java

--- test.java
+++ test.java
@@ -2,3 +2,5 @@ this file is created in local repo
 now i will try to move this in to remote repo
 this time i will add some more lines to this file
 so that i can pull from local repo
+i am copying the same above line again to test fetch function also
+so that i can pull from local repo
